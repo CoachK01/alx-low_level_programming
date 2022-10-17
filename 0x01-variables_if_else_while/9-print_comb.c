432f1bf896b17339e706f74ca6c2bae89394bab9
CoachK01 <olakunleapara@gmail.com>
2022-10-17 13:53:57 -0700

    print_comb.c added

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,21 +6,16 @@
  */
 int main(void)
 {
-	int num;
+	int n;
 
-	for (num = 8; num < = 9; num++)
-	{
+	for (n = 48; n < 58; n++)
 
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-
-		putchar(',');
-		putchar(' ');
-
-	}
-
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	putchar('\n');
-
 	return (0);
 }
