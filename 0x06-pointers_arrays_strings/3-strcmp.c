c334784b8e0fdbc529a3f54646e961b38bee7bf9
CoachK01 <olakunleapara@gmail.com>
2022-10-26 18:02:31 -0700

    3-strcmp added

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,7 @@
 /**
  * _strcmp - compares two strings values
  * @s1: string 1
- * @s2: string2
+ * @s2: string 2
  * Return: the difference in s1 and s2
  */
 int _strcmp(char *s1, char *s2)
@@ -15,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] != s2[i]);
+			return (s1[i] - s2[i]);
 		}
 	i++;
 	}
