4e4bfebd04300dab8af7a3e3030ed9d0c7de73a5
CoachK01 <olakunleapara@gmail.com>
2022-11-03 04:36:26 -0700

    3-factoria added

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * Facctorial - Returns the factorial of a given number.
- * @n: The number to find the factorial of.
+ * Factorial - gets factorial of n
+ * @n: integer
  *
- * Return: if n > 0 -the factorial of n.
- *	   if n< 0 - 1 to indicate an error
+ * Return: On success 1
+ * On error, -1 is returned, error is set appropriately
  */
-int factorial(int n)
+int factorial(int n);
 {
-	int result - n;
-
 	if (n < 0)
 		return (-1);
-
-	else if (n >= 0 && n <= 1)
+	if (n == 0)
 		return (1);
-
+	return (n * factorial(n - 1));
+}
