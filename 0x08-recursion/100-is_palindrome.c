bb87db1283591fe3ca4712f9f8ef0eb4b3f47573
CoachK01 <olakunleapara@gmail.com>
2022-11-03 10:22:14 -0700

    100-is_palindrome added

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * find_strlen - Returns the length of a string'
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s +  len))
+	{
+
