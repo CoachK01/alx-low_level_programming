4b5409d2484dbf2d518f1f60cab3d8d6017f8080
CoachK01 <olakunleapara@gmail.com>
2022-11-14 16:20:23 -0800

    4-new_dog.c added

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -29,7 +29,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	for (i = 0; i < nl; i++)
-		og->name[i] = name[i];
+		dog->name[i] = name[i];
 	dog->age = age;
 	for (ol = 0; owner[ol]; ol++)
 		;
